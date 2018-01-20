b099ea8bfc8111938792786893ed4503adedb915
Salomanuel <salomanuel.aux+code@gmail.com>
2018-01-20 16:40:33 +0100

    add the page for the P5 kerbal

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -14,4 +14,7 @@ class StaticPagesController < ApplicationController
 
 	def squares
 	end
+
+	def kerbal
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
 	get '/contact', 	to: 'static_pages#contact'
 	get '/portfolio',  to: 'static_pages#portfolio'
 	get '/squares',   to: 'static_pages#squares'
+	get '/kerbal', 		to: 'static_pages#kerbal'
 end

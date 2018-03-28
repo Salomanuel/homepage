65038763bd018da41c36d4cd5c4bad9eaa2eabf3
Salomanuel <salomanuel.aux+code@gmail.com>
2018-03-28 13:44:15 +0200

    remove routes to redundant pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 	root 'static_pages#index'
 	
-	get '/about', 				to: 'static_pages#about'
-	get '/contact', 			to: 'static_pages#contact'
+	# get '/about', 				to: 'static_pages#about'
+	# get '/contact', 			to: 'static_pages#contact'
 	get '/portfolio',  		to: 'static_pages#portfolio'
 	get '/ruby-projects', to: 'static_pages#ruby_projects'
 	get '/squares',   		to: 'static_pages#squares'
